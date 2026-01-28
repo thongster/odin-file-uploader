157510b51e602a1d3a4b8b8b430670bb68e27ec9
thongster <106802301+thongster@users.noreply.github.com>
2026-01-28 15:17:17 +0700

    add password hash to prisma schema

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,12 +1,12 @@
-const prisma = require("../lib/prisma");
-const passport = require("../config/passport");
-const bcrypt = require("bcryptjs");
+const prisma = require('../lib/prisma');
+const passport = require('../config/passport');
+const bcrypt = require('bcryptjs');
 
 // express validator
-const { body, validationResult, matchedData } = require("express-validator");
+const { body, validationResult, matchedData } = require('express-validator');
 
 const showIndex = async (req, res) => {
-  res.render("index");
+  res.render('index');
 };
 
 module.exports = { showIndex };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,7 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const indexRouter = Router();
-const indexController = require("../controllers/indexController");
+const indexController = require('../controllers/indexController');
 
-indexRouter.get("/", indexController.showIndex);
+indexRouter.get('/', indexController.showIndex);
 
 module.exports = indexRouter;
