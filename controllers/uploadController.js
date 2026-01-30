f6d4c9adaad3bc3cd40c0972c358a69b2c6e8174
thongster <106802301+thongster@users.noreply.github.com>
2026-01-30 15:22:56 +0700

    Created uploads directory and setting up multer

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,4 +1,17 @@
 const prisma = require('../lib/prisma');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '../uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const uploadMulter = multer({ storage });
 
 const showUpload = async (req, res) => {
   if (!req.isAuthenticated()) {
@@ -9,4 +22,4 @@ const showUpload = async (req, res) => {
 
 const upload = async (req, res) => {};
 
-module.exports = { showUpload, upload };
+module.exports = { showUpload, upload, uploadMulter };
