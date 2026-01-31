87378dace0de1377eb9d8874e55c45c4c40a9d9d
thongster <106802301+thongster@users.noreply.github.com>
2026-01-31 15:07:58 +0700

    create showfolder function and route

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,21 @@ const prisma = require('../lib/prisma');
 const showIndex = async (req, res) => {
   const folders = await prisma.folder.findMany({});
   console.log(folders);
+  console.log(req.user);
   res.render('index', { folders });
 };
 
-module.exports = { showIndex };
+const showFolder = async (req, res) => {
+  const folder = await prisma.folder.findUnique({
+    where: {
+      ownerId_name: {
+        ownerId: req.user.id,
+        name: req.params.folder,
+      },
+    },
+  });
+
+  res.render('files', { folder });
+};
+
+module.exports = { showIndex, showFolder };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,5 +3,6 @@ const indexRouter = Router();
 const indexController = require('../controllers/indexController');
 
 indexRouter.get('/', indexController.showIndex);
+indexRouter.get('/:folder', indexController.showFolder);
 
 module.exports = indexRouter;
