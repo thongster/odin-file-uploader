b53950d0f481a115f368ee6dc3e71ce1e54b7e0f
thongster <106802301+thongster@users.noreply.github.com>
2026-01-31 10:03:15 +0700

    changed schema to include folders, and files to include file size and date created

---
+++ prisma/migrations/20260131030255_add_folders_filesize/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Added the required column `folderId` to the `File` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `mimeType` to the `File` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `size` to the `File` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "File" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "folderId" INTEGER NOT NULL,
+ADD COLUMN     "mimeType" TEXT NOT NULL,
+ADD COLUMN     "size" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
