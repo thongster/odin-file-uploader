fad360ccfea99a15173b91d90f959b669f18b5ef
thongster <106802301+thongster@users.noreply.github.com>
2026-01-31 11:16:01 +0700

    fixed prisma, get folders on index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,12 +1,9 @@
-// const prisma = require('../lib/prisma');
-// const passport = require('../config/passport');
-// const bcrypt = require('bcryptjs');
-
-// // express validator
-// const { body, validationResult, matchedData } = require('express-validator');
+const prisma = require('../lib/prisma');
 
 const showIndex = async (req, res) => {
-  res.render('index');
+  const folders = await prisma.folder.findMany({});
+  console.log(folders);
+  res.render('index', { folders });
 };
 
 module.exports = { showIndex };
