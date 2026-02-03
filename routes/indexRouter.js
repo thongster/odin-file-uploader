5c056c3a09f4f6e29ce346cdcfd740738ec23eed
thongster <106802301+thongster@users.noreply.github.com>
2026-02-03 17:20:04 +0700

    download file in folders

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,7 @@ const indexRouter = Router();
 const indexController = require('../controllers/indexController');
 
 indexRouter.get('/', indexController.showIndex);
-indexRouter.get('/folders', indexController.showFolder);
+indexRouter.get('/folders', indexController.showIndex);
 indexRouter.get('/folders/:folder', indexController.showFolder);
 indexRouter.post('/folders/:folder/rename', indexController.renameFolder);
 indexRouter.post('/folders/:folder/delete', indexController.deleteFolder);
